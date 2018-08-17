4ee83b35842e651b917877dd68b4dce6b0b9ad4d
unknown <abhishekpassan99@gmail.com>
2018-08-17 21:12:19 +0530

    Q3

--- dataStruct/Q3.cpp
+++ dataStruct/Q3.cpp
@@ -4,9 +4,20 @@ using namespace std;
 int revOf(int num);
 
 int main(){
-    cout<<"Enter a Number :";
+    cout<<"\n Enter a Number : ";
     int num;
     cin>>num;
-    cout<<"Reverse of "<<num<<" is : "<<revOf(num);
+    cout<<" Reverse of "<<num<<" is : "<<revOf(num);
     return 0;
+}
+
+int revOf(int num){
+    static int rev = 0;
+    if(num == 0)
+    return rev;
+    else{
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+        revOf(num /= 10);
+    }
 }
