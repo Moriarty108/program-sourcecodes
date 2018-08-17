4e21a00719c8f18394aafd7e5bc842959e8cde2c
unknown <abhishekpassan99@gmail.com>
2018-08-17 20:55:28 +0530

    Q2

---
+++ dataStruct/Q2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+double power(float base , int exponent);
+
+int main(){
+    float base;
+    int expt;
+    cout<<"\n Enter Base : ";
+    cin>>base;
+    cout<<"\n Enter Exponent :";
+    cin>>expt;
+    cout<<"\n Result : "<<power(base , expt);
+}
+
+double power(float base , int exponent){
+    if(exponent == 0)
+    return 1;
+    if(exponent < 0)
+    return power((float)1/base , -exponent);
+    else 
+    return base * power(base , exponent - 1);
+}
