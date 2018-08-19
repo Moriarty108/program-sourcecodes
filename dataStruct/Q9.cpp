d7f0c3b3219de36547e44759eb18b580cf84b923
unknown <abhishekpassan99@gmail.com>
2018-08-19 18:13:14 +0530

    postfix

--- dataStruct/Q9.cpp
+++ dataStruct/Q9.cpp
@@ -1,32 +1,94 @@
-//Remove me when this program is Done!
 #include<iostream>
-#include<string>
-#include<ctype.h>
+#include<string.h>
+#include<math.h>
 #include "Stack.cpp"
 using namespace std;
 
-int cal(int num1 , char op , int num2);
+bool isNumber(char c);
+bool isOperator(char c , char opLib[]);
+float performOperation(int operand1 , int operand2 , char operation);
+float evaluate(string expression);
 
 int main(){
-    char expn[20] = {""};
-    Stack<int> numberStack;
-    Stack<char> operandStack;
+    string expression;
     cout<<"\n Enter an Expression : ";
-    gets(expn);
-    
-    for(int i = 0; expn[i] != '\0'; i++)
-    {
-        if(expn[i] >= '0' && expn[i] <= '9')
-        numberStack.push(expn[i] - 48);
-        else
-        operandStack.push(expn[i]);
-    }
-    int a = numberStack.pop();
-    int b = numberStack.pop();
-    cout<<"\n => Result :"<<cal(b , operandStack.pop() , a);
-    return 0;
+    cin>>expression;
+    cout<<"\n => Result : "<<evaluate(expression)<<endl;
 }
-int cal(int num1 , char op , int num2){
-    if(op == '+')
-    return num1 + num2;
+
+bool isNumber(char c){
+    if(c >= '0' && c <= '9')
+    return true;
+    else return false;
+}
+
+bool isOperator(char c , char opLib[]){
+    bool found = false;
+    for(int i = 0; i < strlen(opLib); i++){
+        if(c == opLib[i]){
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+float performOperation(int operand1 , int operand2 , char operation){
+    
+    switch (operation)
+    {
+        case '+':
+            return operand1 + operand2;
+            break;
+        case '-':
+            return operand1 - operand2;
+            break;
+        case '*':
+            return operand1 * operand2;
+            break;
+        case '/':
+            return (float)operand1 / operand2;
+            break;    
+        case '^':
+            return pow(operand1 , operand2);
+        default: //Enter default case here
+            break;
+    }
+}
+
+float evaluate(string expression){
+    Stack<float> numberStack;
+    //operator Library for storing operators
+    char opLib[] = {'+' , '-' , '*' , '/' , '^'};
+       for(int i = 0; i < expression.length(); i++){
+        if(isNumber(expression[i]))
+        numberStack.push(expression[i] - 48);
+        else if(isOperator(expression[i] , opLib)){
+            if(numberStack.getTOS() >= 1){
+                float num2 = numberStack.pop();
+                float num1 = numberStack.pop();
+                numberStack.push(performOperation(num1 , num2 , expression[i]));
+            }
+            //checking if values provided are enough for operation
+            else if(numberStack.getTOS() < 1){
+                cout<<"\n [Error] Invalid Expression!\n";
+                exit(-1);
+            }
+        }
+        //if user provided any operator which is not supported
+        else{
+            cout<<"\n => [Error] '"<<expression[i]<<"' is an invalid operator\n";
+            exit(-1);
+        }
+        /* Checking if there are more than one result left in stack,
+           if that's the case then the user has not provided the complete expression! 
+        */
+        if(i == (expression.length() - 1)){
+            if(numberStack.getTOS() >= 1){
+                cout<<"\n [Error] Complete Expression Not Provided!\n";
+                exit(-1);
+            }
+        }
+    }   
+    return numberStack.pop();
 }
