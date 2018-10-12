83d14adc96cb83aeb01d0c04a4edf2f623204b4c
Moriarty108 <abhishekpassan99@gmail.com>
2018-10-12 20:46:48 +0530

    Q4a

---
+++ OS/Sep/Q4a.cpp
@@ -0,0 +1,23 @@
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+    pid_t pid;
+    /* fork a child process */
+    pid = fork();
+    if(pid < 0){
+        /* error occurred */
+        fprintf(stderr, "Fork Failed");
+    }
+    else if(pid == 0){
+        /* child process */
+        printf("Child Process\n");
+    }
+    else{
+        /* Parent process */
+        printf("Parent Process\n");
+    }
+    return 0;
+}
